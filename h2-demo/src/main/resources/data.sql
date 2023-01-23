5da9fa7318fe09cd0a01c3514ed43ab3f9b12c64
Valentin Poirot <valentin.poirot@outlook.com>
2023-01-23 10:38:55 +0100

    remove all

--- h2-demo/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO CITY (postcode, name, departement) VALUES ('34000', 'Montpellier', '34');
-INSERT INTO CITY (postcode, name, departement) VALUES ('30100', 'Ales', '30');
-INSERT INTO CITY (postcode, name, departement) VALUES ('28400', 'Nogent-le-Rotrou', '28');
