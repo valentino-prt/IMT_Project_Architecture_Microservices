ff7ff2d64dada92b74f5ce753ed372a8f02bb108
Samuel Ohayon <samuel.ohayon@mines-ales.org>
2023-01-23 11:24:09 +0100

    Created services
    User service
    Hatching service
    Store service

---
+++ hatching/src/main/java/com/example/hatching/HatchingApplication.java
@@ -0,0 +1,13 @@
+package com.example.hatching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HatchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HatchingApplication.class, args);
+	}
+
+}
