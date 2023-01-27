c1b3f26d0818c48063e7a55a0c014f5061fdb00d
Paulin-Zumstein <60385111+Paulin-Zumstein@users.noreply.github.com>
2023-01-27 15:15:48 +0100

    add update feature for gold and xp

--- user/src/main/java/com/example/user/Repository/DresseurRepository.java
+++ user/src/main/java/com/example/user/Repository/DresseurRepository.java
@@ -2,6 +2,9 @@ package com.example.user.Repository;
 
 import com.example.user.Model.Dresseur;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 public interface DresseurRepository extends CrudRepository<Dresseur, Integer> {
+
+    public Dresseur findDresseurById(Integer id);
 }
