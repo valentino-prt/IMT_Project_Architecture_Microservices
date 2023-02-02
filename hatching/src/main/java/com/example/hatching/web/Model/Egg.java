51a6e8ebb26d4ccc3f8062edcfe958322506b470
Valentin Poirot <valentin.poirot@outlook.com>
2023-02-02 22:58:50 +0100

    bug fixed

--- hatching/src/main/java/com/example/hatching/web/Model/Egg.java
+++ hatching/src/main/java/com/example/hatching/web/Model/Egg.java
@@ -7,6 +7,10 @@ import java.util.Date;
 @Entity
 public class Egg {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "egg_generator")
+    @SequenceGenerator(name = "egg_seq", sequenceName = "egg_seq", allocationSize = 1)
+    private Integer id;
     private String name;
     private Integer hatchingTime;
     private Date dateDeposit;
@@ -42,6 +46,14 @@ public class Egg {
 
     public void setHatchingTime(Integer hatchingTime) {
         this.hatchingTime = hatchingTime;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
 }
