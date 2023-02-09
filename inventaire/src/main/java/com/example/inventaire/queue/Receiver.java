54380eb5aa3d2042df2a0c6710a28488d5c86a4c
Samuel Ohayon <samuel.ohayon@mines-ales.org>
2023-02-09 22:46:20 +0100

    Update Receiver.java

--- inventaire/src/main/java/com/example/inventaire/queue/Receiver.java
+++ inventaire/src/main/java/com/example/inventaire/queue/Receiver.java
@@ -21,7 +21,6 @@ public class Receiver {
         StopWatch watch = new StopWatch();
         watch.start();
         System.out.println("[x] Received '" + in + "'");
-
         doWork(in);
         watch.stop();
         System.out.println("[x] Done in " + watch.getTotalTimeSeconds() + "s");
@@ -29,37 +28,8 @@ public class Receiver {
 
     private void doWork(String in) throws InterruptedException, IOException {
         Pokemon pokemon = new Pokemon(in);
-        System.out.println(pokemon.getName() + " " + pokemon.getLevel() + " " + pokemon.getNo());
         if(inventaireService.countPokemon() < 6) {
             inventaireService.savePokemon(pokemon);
-        }
-        else{
-            System.out.println("Do you want to keep the Pokemon ?");
-            BufferedReader reader = new BufferedReader(
-                    new InputStreamReader(System.in));
-
-            // Reading data using readLine
-            String input = reader.readLine();
-            if(input.equals("yes")){
-                List<Pokemon> poke = inventaireService.getAllPokemons();
-
-                for(Pokemon pokemon1 : poke) {
-                    System.out.println(pokemon1.getName() + " " + "lvl" + pokemon1.getLevel() + " Numéro : " + pokemon1.getNo() + " " + pokemon1.getId());
-                }
-                System.out.println("Which Pokemon do you want to delete ? [Enter id]");
-
-                BufferedReader reader1 = new BufferedReader(
-                        new InputStreamReader(System.in));
-
-                Integer input1 = Integer.valueOf(reader1.readLine());
-                inventaireService.deletePokemon(input1);
-                inventaireService.savePokemon(pokemon);
-            }
-            else{
-
-                System.out.println(pokemon.getName() + " not kept");
-            }
-            //afficher les pokémons si oui et rentrer l'id du pokémon avec lequel on veut l"échanger
         }
     }
 }
